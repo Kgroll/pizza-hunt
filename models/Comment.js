9db56bdb766a4eab7d33722ac6bede8397de23de
Kristen Groller <groller@rogers.com>
2021-04-06 21:18:02 -0400

    comment models added

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+const { Schema, nodel } = require('mongoose');
+const { StringDecoder } = require('node:string_decoder');
+const { deleteMany } = require('./Pizza');
+
+const CommentSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    commentBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
